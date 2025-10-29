c235fe83fc95e5fe387cca7ed16eed58457dbc3e
Evans-Asiedu <evansasiedu352@gmail.com>
2025-10-29 13:05:05 +0000

    Define data schema and setup convex and clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
 import React from "react";
@@ -12,11 +13,11 @@ const Home = () => {
     <div className="mt-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Treding Podcast</h1>
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+        {/* <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
           {tasks?.map(({ _id, text }) => (
             <div key={_id}>{text}</div>
           ))}
-        </div>
+        </div> */}
         <div className="podcast_grid">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard
---
+++ convex/users.ts
@@ -0,0 +1,153 @@
+import { ConvexError, v } from "convex/values";
+
+import { internalMutation, query, mutation } from "./_generated/server";
+
+export const getUserById = query({
+  args: { clerkId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    return user;
+  },
+});
+
+// this query is used to get the top user by podcast count. first the podcast is sorted by views and then the user is sorted by total podcasts, so the user with the most podcasts will be at the top.
+export const getTopUserByPodcastCount = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const user = await ctx.db.query("users").collect();
+
+    const userData = await Promise.all(
+      user.map(async (u) => {
+        const podcasts = await ctx.db
+          .query("podcasts")
+          .filter((q) => q.eq(q.field("user"), u._id))
+          .collect();
+
+        const sortedPodcasts = podcasts.sort((a, b) => b.views - a.views);
+
+        return {
+          ...u,
+          totalPodcasts: podcasts.length,
+          podcast: sortedPodcasts.map((p) => ({
+            podcastTitle: p.podcastTitle,
+            podcastId: p._id,
+          })),
+        };
+      })
+    );
+
+    return userData.sort((a, b) => b.totalPodcasts - a.totalPodcasts);
+  },
+});
+
+export const createUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      email: args.email,
+      imageUrl: args.imageUrl,
+      name: args.name,
+      //   playlist: [],
+    });
+  },
+});
+
+export const updateUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    imageUrl: v.string(),
+    email: v.string(),
+  },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.patch(user._id, {
+      imageUrl: args.imageUrl,
+      email: args.email,
+    });
+
+    const podcast = await ctx.db
+      .query("podcasts")
+      .filter((q) => q.eq(q.field("authorId"), args.clerkId))
+      .collect();
+
+    await Promise.all(
+      podcast.map(async (p) => {
+        await ctx.db.patch(p._id, {
+          authorImageUrl: args.imageUrl,
+        });
+      })
+    );
+  },
+});
+
+export const deleteUser = internalMutation({
+  args: { clerkId: v.string() },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.delete(user._id);
+  },
+});
+
+export const updateUserPlaylist = mutation({
+  args: {
+    clerkId: v.string(),
+    podcastId: v.id("podcasts"),
+  },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    // const playlist = user?.playlist || [];
+
+    // Add the new podcast ID to the playlist, avoiding duplicates
+    // if (!playlist.includes(args.podcastId)) {
+    //   playlist.push(args.podcastId);
+
+    //   // Update the user's playlist field
+    //   await ctx.db.patch(user._id, {
+    //     playlist,
+    //   });
+
+    //   return { message: "Added to your playlist" };
+    // }
+    //  else {
+    //   return { message: "Already in the playList" };
+    // }
+  },
+});
